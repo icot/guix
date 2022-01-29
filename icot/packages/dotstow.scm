77c61f30f475a7ab5bf37d1f2a68abba86125ffc
icot <icot@users.noreply.github.com>
2022-01-29 14:32:25 +0100

    dostow: define-module

--- icot/packages/dotstow.scm
+++ icot/packages/dotstow.scm
@@ -1,10 +1,29 @@
-(use-modules (guix packages)
-             (guix gexp)
-             (guix git-download)
-             (guix build-system guile)
-             (guix build-system copy)
-     	     (gnu packages guile)
-             (guix licenses))
+;;; GNU Guix -- Functional package management for GNU
+;;; Copyright © 2022 Ignacio Coterillo <ignacio.coterillo@gmail.com>
+;;;
+;;; This file is NOT part of GNU Guix.
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (icot packages dotstow)
+  #:use-modules (guix packages)
+  #:use-modules (guix gexp)
+  #:use-modules (guix git-download)
+  #:use-modules (guix build-system guile)
+  #:use-modules (guix build-system copy)
+  #:use-modules (gnu packages guile)
+  #:use-modules (guix licenses))
 
 (define-public dotstow
   (package
